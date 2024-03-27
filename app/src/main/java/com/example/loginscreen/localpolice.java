f3a5cafe2ffeedf69db84e8017ffac542a27fa30
Dharmendra Amarsingh Vaghela <testthatcodes@gmail.com>
2024-03-27 19:18:53 +0530

    add calling function

--- app/src/main/java/com/example/loginscreen/localpolice.java
+++ app/src/main/java/com/example/loginscreen/localpolice.java
@@ -2,6 +2,7 @@ package com.example.loginscreen;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +39,9 @@ public class localpolice extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(localpolice.this, "Calling Police", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+91100"));
+                startActivity(intent);
             }
         });
 
@@ -45,18 +49,27 @@ public class localpolice extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(localpolice.this, "Calling Ambulance", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+91108"));
+                startActivity(intent);
             }
         });
         btnFirePolice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(localpolice.this, "Calling Fire Police", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+91101"));
+                startActivity(intent);
             }
         });
         btnWomenSafety.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(localpolice.this, "Calling Women Safety Helpline", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:4856135435"));
+                startActivity(intent);
             }
         });
 
@@ -64,6 +77,11 @@ public class localpolice extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(localpolice.this, "Calling Child Helpline", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("smsto:"+Uri.encode("+911098")));
+                intent.putExtra("sms_body","hello how are you");
+
+                startActivity(intent);
             }
         });
 
@@ -71,6 +89,11 @@ public class localpolice extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(localpolice.this, "Hospital Booked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("smsto:"+Uri.encode("+9158641635486")));
+                intent.putExtra("sms_body","hello how are you");
+
+                startActivity(intent);
             }
         });
 
