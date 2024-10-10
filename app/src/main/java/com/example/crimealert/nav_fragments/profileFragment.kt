475372e3fa2bcd4784e9956d194a15559f6d54f5
mohitsingh316 <mohitsingh79897@gmail.com>
2024-10-10 07:59:16 +0530

    Adding all files including ignored ones

--- app/src/main/java/com/example/crimealert/nav_fragments/profileFragment.kt
+++ app/src/main/java/com/example/crimealert/nav_fragments/profileFragment.kt
@@ -1,12 +1,15 @@
-package com.example.crimealert.bottom_fragments
+package com.example.crimealert.nav_fragments
 
+import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
@@ -16,38 +19,43 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.crimealert.R
-import com.google.android.gms.cast.framework.media.ImagePicker
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.InputStream
-import java.util.UUID
+import java.util.*
 
-class profile : Fragment() {
 
+class SettingFragment : Fragment() {
     private lateinit var profileImage: ImageView
     private lateinit var nameTextView: TextView
     private lateinit var mobileNoTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var communityTextView: TextView
     private lateinit var editButton: ImageButton
 
     private lateinit var firestore: FirebaseFirestore
     private lateinit var mAuth: FirebaseAuth
     private lateinit var storageReference: FirebaseStorage
 
+    private lateinit var photoURI: Uri
     private val REQUEST_IMAGE_CAPTURE = 1
     private val REQUEST_IMAGE_PICK = 2
+    private val REQUEST_CAMERA_PERMISSION = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         firestore = FirebaseFirestore.getInstance()
         mAuth = FirebaseAuth.getInstance()
         storageReference = FirebaseStorage.getInstance()
-
     }
 
     override fun onCreateView(
@@ -59,11 +67,25 @@ class profile : Fragment() {
         profileImage = view.findViewById(R.id.profile_image)
         nameTextView = view.findViewById(R.id.name)
         mobileNoTextView = view.findViewById(R.id.mobile_no)
+        emailTextView = view.findViewById(R.id.email)
+        communityTextView = view.findViewById(R.id.community)
         editButton = view.findViewById(R.id.image_edit)
 
         loadUserProfile()
+
         editButton.setOnClickListener {
-            showImagePickerOptions()
+            if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+                // Request Camera Permission
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CAMERA_PERMISSION
+                )
+            } else {
+                // Show image picker options
+                showImagePickerOptions()
+            }
         }
 
         return view
@@ -77,10 +99,14 @@ class profile : Fragment() {
                 if (document != null && document.exists()) {
                     val name = document.getString("name")
                     val mobileNo = document.getString("mobileNo")
+                    val email = document.getString("email")
+                    val community = document.get("joined_community") as? List<String> ?: emptyList()
                     val imageUrl = document.getString("imageUrl") ?: DEFAULT_PROFILE_IMAGE_URL
 
                     nameTextView.text = name
                     mobileNoTextView.text = mobileNo
+                    emailTextView.text = email
+                    communityTextView.text = community.joinToString(", ")
                     Glide.with(this)
                         .load(imageUrl)
                         .placeholder(R.drawable.ic_profile) // Placeholder image
@@ -89,18 +115,17 @@ class profile : Fragment() {
                 }
             }
             .addOnFailureListener { e ->
-                // Handle the error
+                Toast.makeText(requireContext(), "Error loading profile", Toast.LENGTH_SHORT).show()
             }
     }
 
     private fun showImagePickerOptions() {
-        val options = arrayOf("Take Photo", "Choose from Gallery")
+        val options = arrayOf("Choose from Gallery")
         AlertDialog.Builder(requireContext())
             .setTitle("Select Image")
             .setItems(options) { _, which ->
                 when (which) {
-                    0 -> dispatchTakePictureIntent()
-                    1 -> dispatchPickPictureIntent()
+                    0 -> dispatchPickPictureIntent()
                 }
             }
             .show()
@@ -109,8 +134,26 @@ class profile : Fragment() {
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (takePictureIntent.resolveActivity(requireContext().packageManager) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            val photoFile = createImageFile()
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(
+                    requireContext(),
+                    "${requireContext().packageName}.provider",
+                    photoFile
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
+    }
+
+    private fun createImageFile(): File? {
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${System.currentTimeMillis()}_",
+            ".jpg",
+            storageDir
+        )
     }
 
     private fun dispatchPickPictureIntent() {
@@ -121,26 +164,20 @@ class profile : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
-            val imageUri = data?.data
-            if (requestCode == REQUEST_IMAGE_CAPTURE) {
-                val bitmap = data?.extras?.get("data") as? Bitmap
-                bitmap?.let {
-                    handleImage(bitmap)
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE -> {
+                    handleImageFromUri(photoURI)
                 }
-            } else if (requestCode == REQUEST_IMAGE_PICK) {
-                imageUri?.let {
-                    handleImageFromUri(it)
+                REQUEST_IMAGE_PICK -> {
+                    val imageUri = data?.data
+                    imageUri?.let {
+                        handleImageFromUri(it)
+                    }
                 }
             }
         } else {
             Toast.makeText(requireContext(), "Image selection cancelled", Toast.LENGTH_SHORT).show()
         }
-    }
-
-    private fun handleImage(bitmap: Bitmap) {
-        val croppedBitmap = cropImage(bitmap)
-        uploadImageToFirebase(croppedBitmap)
-        profileImage.setImageBitmap(croppedBitmap)
     }
 
     private fun handleImageFromUri(uri: Uri) {
@@ -183,16 +220,15 @@ class profile : Fragment() {
                     }
                     .addOnFailureListener { e ->
                         Toast.makeText(requireContext(), "Error uploading image", Toast.LENGTH_SHORT).show()
-                        Log.d("TAG", "Error uploading image: ${e.message}")
                     }
             }
         }.addOnFailureListener {
             Toast.makeText(requireContext(), "Error uploading image", Toast.LENGTH_SHORT).show()
-            Log.d("TAG", "Error uploading image: ${it.message}")
         }
     }
 
     companion object {
         private const val DEFAULT_PROFILE_IMAGE_URL = "https://firebasestorage.googleapis.com/v0/b/crimealert-20d52.appspot.com/o/profile.png?alt=media&token=a25bbbff-d8ee-4bc1-962f-9b52e814f38a"
     }
+
 }
